a2bcfea799cc9a7db93e6ebd31ca64aa87e1abed
EQJ168 <eeqj-pm22@student.tarc.edu.my>
2024-09-14 14:37:20 +0800

    Update Assignment_Collaborative_Filtering.py

--- Assignment_Collaborative_Filtering.py
+++ Assignment_Collaborative_Filtering.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import streamlit as st
 
-st.title('Game Recommender System (Item-Based Collaborative Filtering)')
+st.title('Games You May Like (Personalized Recommendations)')
 
-@st.cache  
+@st.cache
 def load_data():
     path = 'NewDataSet.csv'  # Game dataset
     df = pd.read_csv(path)
@@ -19,34 +19,51 @@ data = load_data()
 # Creating the user-score matrix for collaborative filtering
 score_matrix = data.pivot_table(index='user_id', columns='Title', values='user_score', fill_value=0)
 
-# List of game titles available
-game_titles = score_matrix.columns.sort_values().tolist()
+# List of users available
+user_ids = score_matrix.index.sort_values().tolist()
 
-# User selects a game title for recommendation
-game_title = st.selectbox("Select a game title to get recommendations", game_titles)
+# Select a user to provide recommendations
+user_id = st.selectbox("Select your user ID to get personalized game recommendations", user_ids)
 
-if game_title:
-    # Retrieving the user scores for the selected game
-    game_user_score = score_matrix[game_title]
-
-    # Calculating the correlation of the selected game with other games
-    similar_to_game = score_matrix.corrwith(game_user_score)
+if user_id:
+    # Retrieve the user's ratings
+    user_ratings = score_matrix.loc[user_id]
     
-    # Creating a DataFrame to store the correlations
-    corr_drive = pd.DataFrame(similar_to_game, columns=['Correlation']).dropna()
+    # Filtering out games the user has not rated (where score is 0)
+    user_ratings = user_ratings[user_ratings > 0]
 
-    # Displaying top similar games based on correlation
-    st.subheader(f"Games similar to '{game_title}':")
-    st.dataframe(corr_drive.sort_values('Correlation', ascending=False).head(10))
+    st.subheader(f"Games you've rated:")
+    st.write(user_ratings.sort_values(ascending=False))
 
-    # Filtering games that have more than 10 user ratings
+    # Find games that are similar to the ones the user has rated
+    similar_games = pd.Series()
+    
+    for game in user_ratings.index:
+        # Get correlation for each rated game
+        similar_to_game = score_matrix.corrwith(score_matrix[game])
+        
+        # Create a Series for each game and add it to the list of similar games
+        similar_games = similar_games.append(similar_to_game)
+
+    # Group by game title and sum up the correlations
+    similar_games = similar_games.groupby(similar_games.index).mean()
+    
+    # Remove games the user has already rated
+    similar_games = similar_games.drop(user_ratings.index, errors='ignore')
+
+    # Convert the Series to DataFrame and sort by correlation
+    recommended_games = pd.DataFrame(similar_games, columns=['Correlation']).dropna().sort_values('Correlation', ascending=False)
+    
+    st.subheader("Games You May Like (Recommended):")
+    st.dataframe(recommended_games.head(10))
+
+    # Filtering games that have more than 10 user ratings for better recommendations
     user_scores_count = data.groupby('Title')['user_score'].count().rename('total num_of_user_score')
-    merged_corr_drive = corr_drive.join(user_scores_count, how='left')
+    merged_corr_drive = recommended_games.join(user_scores_count, how='left')
 
-    # Displaying games with high score correlations and more than 10 ratings
     st.subheader("Top Recommended Games with Sufficient Ratings:")
     high_score_corr = merged_corr_drive[merged_corr_drive['total num_of_user_score'] > 10].sort_values('Correlation', ascending=False).head()
     st.dataframe(high_score_corr)
 
 else:
-    st.info('Please select a game title from the dropdown to get recommendations.')
+    st.info('Please select your user ID to get recommendations.')
